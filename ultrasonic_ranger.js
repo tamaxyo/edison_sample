c94dc8099722e3a6a13de69202b64618bab94eb0
tamaxyo <tamaxyo@gmail.com>
2014-11-15 22:52:52 +0900

    ultrasonic trial

--- ultrasonic_ranger.js
+++ ultrasonic_ranger.js
@@ -2,8 +2,10 @@ console.log("start")
 
 var m = require('mraa');
 
-var x = new m.Gpio(5);
-x.dir(m.DIR_IN);
+var pwm5 = new m.Pwm(5, -1, false);
+pwm5.enable(true);
+
+pwm5.period_us(2000);
 
 var cnt = 0;
 var print = function() {
