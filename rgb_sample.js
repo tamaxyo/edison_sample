3580b9c3c1040985e6dc65e57d38ac3c3b7f46c3
tamaxyo <tamaxyo@gmail.com>
2014-11-15 21:33:30 +0900

    change color of lcd

--- rgb_sample.js
+++ rgb_sample.js
@@ -8,7 +8,16 @@ x.address(0x62)
 x.writeReg(0,  0)
 x.writeReg(1,  0)
 
-x.writeReg(char('0x08'),  char('0xAA'))
-x.writeReg(char('0x04'),  255)
-x.writeReg(char('0x02'),  255)
+var val = 255;
+var step = 16;
+var fn = function() {
+  if(val == 0) {
+    return
+  }
+  val -= step;
+  x.writeReg(char('0x08'),  char('0xAA'))
+  x.writeReg(char('0x04'),  val)
+  x.writeReg(char('0x02'),  val)
+};
+fn();
 
