9088da27415d353722d2dce2183670c1a8975dc2
tamaxyo <tamaxyo@gmail.com>
2014-11-15 22:37:51 +0900

    ultrasonic trial

---
+++ ultrasonic_ranger.js
@@ -0,0 +1,16 @@
+var m = require('mraa');
+
+var x = new m.Gpio(5);
+x.dir(m.DIR_IN);
+
+var cnt = 0;
+var print = function() {
+  if(cnt == 10) {
+    return
+  }
+  cnt++;
+  console.log(x.read());
+  setTimeout(print, 1000);
+}
+
+
