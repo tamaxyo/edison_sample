5bed491c619c6206c7f4b640fd986349fea7d2c7
tamaxyo <tamaxyo@gmail.com>
2014-11-16 15:28:45 +0900

    update

--- bt.js
+++ bt.js
@@ -3,12 +3,11 @@ var uuid = "e2c56db5dffb48d2b060d0f5a71096e0";
 var major = 0;
 var minor = 0;
 var power = -59;
+var advertising = false;
 
 var m = require('mraa');
 var d = new m.Gpio(8);
-d.dir(m.DIR_IN);
-
-var advertising = false;
+d.dir(m.DIR_OUT);
 
 var advertise = function() {
   b.stopAdvertising();
@@ -16,14 +15,16 @@ var advertise = function() {
 }
 
 advertise();
-
 var fn = function() {
   var danger = d.read();
-  if(danger == 1 && !advertising) {
+  console.log("danger: ", danger);
+  if(danger != 0 && !advertising) {
+    console.log("danger!");
     major = 1;
     advertise = true;
     advertise();
   } else if(danger == 0 && advertising) {
+    console.log("become safe");
     major = 0;
     advertise = false;
     advertise();
